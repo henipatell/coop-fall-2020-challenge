d23f3a85e20d2dedc6a3c5f7a2d46f4c98b86007
henipatell <henipatel8913@gmail.com>
2020-07-10 14:55:24 -0400

    fall 2020 co-op challage code file modified

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,45 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.totalSteps = 0
+        self.history = []
+        #["Add", "Substract", "Undo", "Redo", "Bulk_Undo", "Bulk_Redo"]
 
     def add(self, num: int):
-        pass
+        self.value = self.value + num
+        self.totalSteps += 1;
+        self.history.append(self.value)
 
     def subtract(self, num: int):
-        pass
+        self.value = self.value - num
+        self.totalSteps += 1;
+        self.history.append(self.value)
 
     def undo(self):
-        pass
+        if self.totalSteps > 0:
+            self.totalSteps -= 1
+            self.history.pop()
+        else:
+            print("Nothing to undo")
+        self.totalSteps += 1;
 
     def redo(self):
-        pass
+        if self.totalSteps > 0:
+            self.totalSteps -= 1
+            temp = self.history.pop()
+            self.history.append(temp)
+        else:
+            print("Nothing to undo")
+        self.totalSteps += 1
 
     def bulk_undo(self, steps: int):
-        pass
+        while steps>0:
+            self.undo()
+            steps = steps - 1
+        self.totalSteps += 1
 
     def bulk_redo(self, steps: int):
-        pass
+        while steps>0:
+            self.redo()
+            steps = steps - 1
+        self.totalSteps += 1
